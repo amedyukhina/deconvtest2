23367cbac4840447df304d5dda291e4e329c35a9
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-02-04 17:00:46 -0600

    adding Poisson noise

--- deconvtest2_modules/tests/unit/test_transforms.py
+++ deconvtest2_modules/tests/unit/test_transforms.py
@@ -3,7 +3,8 @@ import unittest
 import numpy as np
 from ddt import ddt, data
 
-from ...transforms.transform_modules import convolve
+from ...transforms.transform_modules import convolve, poisson_noise
+
 
 @ddt
 class TestConvolve(unittest.TestCase):
@@ -25,6 +26,31 @@ class TestConvolve(unittest.TestCase):
     def test_error(self, case):
         img, psf = case
         self.assertRaises(ValueError, convolve, img, psf)
+
+
+@ddt
+class TestPoisson(unittest.TestCase):
+
+    @data(
+        [np.ones([10, 10, 10]), 3],
+        [np.ones([10, 10]), 10],
+    )
+    def test_dimensions(self, case):
+        img, snr = case
+        self.assertEqual(len(poisson_noise(img, snr).shape), len(img.shape))
+
+    @data(
+        [np.ones([10, 10, 10]), 3],
+        [np.ones([10, 10, 10]), 1],
+        [np.ones([10, 10, 10]), 2],
+        [np.ones([10, 10, 10]), 15],
+        [np.ones([10, 10]), 10],
+    )
+    def test_snr(self, case):
+        img, snr = case
+        out = poisson_noise(img, snr)
+        out_snr = img.max()/np.std(out[np.where(img > 0)])
+        self.assertGreaterEqual(0.1, abs(out_snr - snr)/snr)
 
 
 if __name__ == '__main__':
