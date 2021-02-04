be8ea4599d0d824ae60eed8dfceca274a387578f
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-02-04 14:35:44 -0600

    renamed voxel size to size

--- deconvtest2_core/tests/unit/test_conversions.py
+++ deconvtest2_core/tests/unit/test_conversions.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 from ddt import ddt, data
 
-from ...utils.conversion import convert_voxel_size
+from ...utils.conversion import convert_size
 
 
 @ddt
@@ -12,19 +12,13 @@ class TestConversion(unittest.TestCase):
         2, [3], [1, 2], np.ones(2), np.ones(3), [1, 2, 3],
     )
     def test_valid_voxel_size(self, x):
-        self.assertEqual(len(convert_voxel_size(x)), 3)
-
-    @data(
-        'test', 's',
-    )
-    def test_invalid_voxe_size_type(self, x):
-        self.assertRaises(TypeError, convert_voxel_size, x)
+        self.assertEqual(len(convert_size(x)), 3)
 
     @data(
         [], np.ones(10), [1, 2, 3, 4],
     )
     def test_invalid_voxel_size_length(self, x):
-        self.assertRaises(ValueError, convert_voxel_size, x)
+        self.assertRaises(ValueError, convert_size, x)
 
 
 if __name__ == '__main__':
--- deconvtest2_core/utils/conversion.py
+++ deconvtest2_core/utils/conversion.py
@@ -3,30 +3,28 @@ from typing import Union
 import numpy as np
 
 
-def convert_voxel_size(voxel_size: Union[float, list, np.ndarray]):
+def convert_size(size: Union[float, list, np.ndarray]):
     """
-    Convert voxel size into a uniform format of 3-number array.
+    Convert voxel size or object dimension into a uniform format of 3-number array.
 
     Parameters
     ----------
-    voxel_size : scalar or sequence
-        Voxel size in z, y and x used to generate the PSF image.
-        If one value is provided, the voxel size is assumed to be equal along all axes.
+    size : scalar or sequence
+        Voxel size or object dimensions in z, y and x.
+        If one value is provided, the size is assumed to be equal along all axes.
 
     Returns
     -------
     numpy.array
         Formatted voxel size
     """
-    if not type(voxel_size) in [float, int, list, np.ndarray]:
-        raise TypeError("'voxel_size' must be number or sequence, '{}' provided!".format(type(voxel_size).__name__))
-    voxel_size = np.array([voxel_size]).flatten()
-    if len(voxel_size) == 1:
-        voxel_size = np.array([voxel_size[0]] * 3)
-    elif len(voxel_size) == 2:
-        voxel_size = np.array([voxel_size[0], voxel_size[1], voxel_size[1]])
-    elif len(voxel_size) == 3:
-        voxel_size = voxel_size
+    size = np.array([size]).flatten()
+    if len(size) == 1:
+        size = np.array([size[0]] * 3)
+    elif len(size) == 2:
+        size = np.array([size[0], size[1], size[1]])
+    elif len(size) == 3:
+        size = size
     else:
-        raise ValueError('voxel size must be a number of a sequence of length 2 or 3!')
-    return voxel_size
+        raise ValueError('Size must be a number of a sequence of length 2 or 3!')
+    return size
