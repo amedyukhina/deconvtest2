70b1e59fe84b155d59102263474d15e691dbcded
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-02-04 16:40:37 -0600

    bug fix

--- deconvtest2_core/utils/utils.py
+++ deconvtest2_core/utils/utils.py
@@ -3,6 +3,6 @@ import numpy as np
 
 def check_type(names, variables, types):
     for name, var, t in zip(names, variables, types):
-        t = np.array([t]).flatten()
+        t = list(np.array([t]).flatten())
         if not type(var) in t:
             raise TypeError("Type of '{}' must be one of: {}; '{}' provided!".format(name, t, type(var).__name__))
