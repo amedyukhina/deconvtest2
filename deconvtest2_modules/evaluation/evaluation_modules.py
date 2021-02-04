066f3e55f4d0ca73302ec6e895c622977f81cad5
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-02-04 17:26:43 -0600

    RMSE computation

---
+++ deconvtest2_modules/evaluation/evaluation_modules.py
@@ -0,0 +1,31 @@
+from deconvtest2_core.utils.conversion import unify_shape
+import numpy as np
+from deconvtest2_core.utils.utils import check_type
+
+
+def rmse(img1: np.ndarray, img2: np.ndarray) -> float:
+    """
+    Compute Root Mean Square Error (RMSE) between two input images.
+
+    Parameters
+    ----------
+    img1 : ndarray
+        Input ground truth image.
+        This image volume is used to normalize the output RMSE
+    img2 : numpy.ndarray
+        Second input image.
+
+    Returns
+    -------
+    float:
+        RMSE between the two input images
+    """
+    volume = np.prod(img1.shape)
+    check_type(['img1', 'img2'],
+               [img1, img2],
+               [np.ndarray]*2)
+    img1, img2 = unify_shape(img1, img2)
+    rmse = np.sqrt(np.sum((img1 - img2)**2) / volume)
+    return rmse
+
+
