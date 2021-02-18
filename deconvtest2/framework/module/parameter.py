19c6a6c7c11c2a3c5b79779bf91d103ad62fd6a0
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-02-18 17:55:46 -0600

    add `optional` variable; __repr__ method

--- deconvtest2/framework/module/parameter.py
+++ deconvtest2/framework/module/parameter.py
@@ -4,7 +4,7 @@ class Parameter:
     """
     Abstract parameter class
     """
-    def __init__(self, name, default_value=None, value=None, parameter_type=None, doc=''):
+    def __init__(self, name, default_value=None, value=None, optional=True, parameter_type=None, doc=''):
         """
 
         Parameters
@@ -17,6 +17,9 @@ class Parameter:
         value : type specified by `parameter_type`, optional
             Parameter value.
             Default is None.
+        optional : bool, optional
+            If True, the parameter is optional.
+            Default is True
         parameter_type : type or typing.Union, optional
             Parameter type.
             Default is None.
@@ -28,5 +31,13 @@ class Parameter:
         self.default_value = default_value
         self.value = value
         self.type = parameter_type
+        self.optional = optional
         self.__doc__ = doc + '\n----\nvariables:\n- name\n- default_value\n- value\n- type'
+
+    def __repr__(self):
+        return "Parameter: name={}, value={}, type={}, optional={}, default_value={}".format(self.name,
+                                                                                             self.value,
+                                                                                             self.type,
+                                                                                             self.optional,
+                                                                                             self.default_value)
 
