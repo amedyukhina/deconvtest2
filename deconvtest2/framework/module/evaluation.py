75527815d00edadbac0b42afe5fe6035ef6badef
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-11 11:47:02 -0600

    rename

---
+++ deconvtest2/framework/module/evaluation.py
@@ -0,0 +1,12 @@
+from ..module.module import Module
+
+
+class Evaluation(Module):
+    """
+    Evaluation module
+    """
+
+    def __init__(self, method, parameters: dict = None):
+        super(Evaluation, self).__init__(method=method,
+                                         parameters=parameters,
+                                         parent_name='deconvtest2.modules.evaluation')
