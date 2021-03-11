295796acb3a768d714249d405c037a8e3c71f4c2
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-11 11:47:13 -0600

    unit tests for module classes

--- deconvtest2/tests/unit_framework/test_module.py
+++ deconvtest2/tests/unit_framework/test_module.py
@@ -3,7 +3,11 @@ import unittest
 import numpy as np
 from ddt import ddt, data
 from deconvtest2.framework.module.deconvolution import Deconvolution
+from deconvtest2.framework.module.evaluation import Evaluation
+from deconvtest2.framework.module.ground_truth import GroundTruth
 from deconvtest2.framework.module.module import Module
+from deconvtest2.framework.module.psf import PSF
+from deconvtest2.framework.module.transform import Transform
 
 
 @ddt
@@ -22,6 +26,34 @@ class TestModuleImport(unittest.TestCase):
     )
     def test_deconvolution_method(self, method):
         module = Deconvolution(method)
+        self.assertIsNotNone(module.method)
+
+    @data(
+        'gaussian',
+    )
+    def test_psf_method(self, method):
+        module = PSF(method)
+        self.assertIsNotNone(module.method)
+
+    @data(
+        'ellipsoid',
+    )
+    def test_gt_method(self, method):
+        module = GroundTruth(method)
+        self.assertIsNotNone(module.method)
+
+    @data(
+        'convolve',
+    )
+    def test_transform_method(self, method):
+        module = Transform(method)
+        self.assertIsNotNone(module.method)
+
+    @data(
+        'rmse',
+    )
+    def test_accuracy_method(self, method):
+        module = Evaluation(method)
         self.assertIsNotNone(module.method)
 
     @data(
