ddea5712541a5034825dc00c080c0b4d989d0301
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-11 18:39:37 -0600

    adding a workflow step

--- deconvtest2/framework/workflow/workflow.py
+++ deconvtest2/framework/workflow/workflow.py
@@ -18,3 +18,18 @@ class Workflow:
     def __init__(self, name : str = 'New Workflow'):
         self.name = name
         self.available_steps = list_available_steps()
+        self.steps = []
+
+    def add_step(self, name, method=None):
+        step = None
+        for st in self.available_steps:
+            if st.name == name:
+                step = st
+        if step is None:
+            raise ValueError(rf'{name} is not a valid step! Valid steps are {self.available_steps}')
+        if method is not None:
+            step.add_method(method)
+        self.steps.append(step)
+
+
+
