970c1ee39f363865d26d4819a58daa033196202a
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-11 18:39:52 -0600

    unit tests for Workflow and Step classes

--- deconvtest2/tests/unit_framework/test_workflow.py
+++ deconvtest2/tests/unit_framework/test_workflow.py
@@ -1,8 +1,10 @@
 import unittest
+import warnings
 
 from ddt import ddt, data
 
 from ...framework.workflow.step import Step
+from ...framework.workflow.workflow import Workflow
 
 
 @ddt
@@ -15,7 +17,7 @@ class TestStep(unittest.TestCase):
         'Transform',
         'Deconvolution'
     )
-    def test_method(self, method):
+    def test_step(self, method):
         step = Step(method)
         self.assertIsNotNone(step.step)
 
@@ -23,8 +25,35 @@ class TestStep(unittest.TestCase):
         'wrong_step1',
         'wrong_step2'
     )
-    def test_method(self, method):
+    def test_wrong_step(self, method):
         self.assertRaises(ValueError, Step, method)
+
+    def test_list_workflow_steps(self):
+        w = Workflow()
+        steps = [st.name for st in w.available_steps]
+        self.assertIn('PSF', steps)
+
+    def test_list_step_methods(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            s = Step("PSF")
+            self.assertIn('gaussian', s.list_available_methods())
+
+    def test_add_method(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            s = Step('PSF')
+            s.add_method('gaussian')
+            self.assertIsNotNone(s.method)
+
+    def test_add_wrong_method(self):
+        s = Step('PSF')
+        self.assertRaises(ValueError, s.add_method, 'wrong_method')
+
+    def test_add_step(self):
+        w = Workflow()
+        w.add_step('PSF', 'gaussian')
+        self.assertEqual(len(w.steps), 1)
 
 
 if __name__ == '__main__':
