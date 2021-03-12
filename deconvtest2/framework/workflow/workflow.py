1041f092fd537280b4774988932c93e36855a9c9
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-11 18:31:16 -0600

    class for Workflow

--- deconvtest2/framework/workflow/workflow.py
+++ deconvtest2/framework/workflow/workflow.py
@@ -1,12 +1,20 @@
+import inspect
+
 from ...core.utils.utils import list_modules
 from ...framework import step
+from .step import Step
 
 
-def list_steps():
-    steps = list_modules(step)
+def list_available_steps():
+    steps = list_modules(step, module_type=inspect.isclass)
+    steps = [Step(st[0].__name__) for st in steps]
     return steps
 
 
-if __name__ == '__main__':
-    steps = list_steps()
-    print(steps)
+class Workflow:
+    """
+    Workflow class
+    """
+    def __init__(self, name : str = 'New Workflow'):
+        self.name = name
+        self.available_steps = list_available_steps()
