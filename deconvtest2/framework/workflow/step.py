4a8d4488f4d4bd920962f3d2f053b0fb60f65a62
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-18 12:22:32 -0500

    list parameters

--- deconvtest2/framework/workflow/step.py
+++ deconvtest2/framework/workflow/step.py
@@ -1,7 +1,8 @@
 import inspect
+import warnings
 
 from ...core.utils.utils import list_modules
-from ...framework import step
+from ...framework import step as workflow_steps
 
 
 class Step:
@@ -13,11 +14,12 @@ class Step:
         self.name = step_name
         self.method = method
 
-        steps = list_modules(step, module_type=inspect.isclass)
+        steps = list_modules(workflow_steps, module_type=inspect.isclass)
         self.step = None
         for st in steps:
             if st[0].__name__ == step_name:
                 self.step = st[0]
+
         if self.step is None:
             raise ValueError(
                 rf'{step_name} is not a valid step! Valid steps are: {[st[0].__name__ for st in steps]}')
@@ -37,3 +39,11 @@ class Step:
             self.method = method
         else:
             raise ValueError(rf'{method} is not a valid method! Valid methods are: {self.list_available_methods()}')
+
+    def list_parameters(self):
+        if self.method is None:
+            warnings.warn(rf'No method is defined for step {self.name} to list parameters!')
+            return None
+        else:
+            module = self.step(self.method)
+            return module.parameters
