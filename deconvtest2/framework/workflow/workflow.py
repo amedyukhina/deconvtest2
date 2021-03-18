815db22e8ad14693fce94b2c1a24a420bb4305b3
Medyukhina, Anna <Anna.Medyukhina@STJUDE.ORG>
2021-03-18 18:03:49 -0500

    export workflow to json

--- deconvtest2/framework/workflow/workflow.py
+++ deconvtest2/framework/workflow/workflow.py
@@ -1,4 +1,6 @@
 import inspect
+import json
+import os
 
 from ...core.utils.utils import list_modules
 from ...framework import step as workflow_steps
@@ -19,9 +21,29 @@ class Workflow:
         self.name = name
         self.available_steps = list_available_steps()
         self.steps = []
+        self.path = None
 
     def add_step(self, step: Step):
         self.steps.append(step)
 
+    def to_dict(self):
+        workflow = dict()
+        workflow['name'] = self.name
+        workflow['path'] = self.path
+        workflow['steps'] = [step.to_dict() for step in self.steps]
+        return workflow
 
+    def __repr__(self):
+        return json.dumps(self.to_dict(), indent=4)
 
+    def save(self, path: str = None):
+        if path is not None:
+            self.path = path
+
+        if self.path is None:
+            raise ValueError('Path must be provided!')
+        else:
+            if not os.path.exists(os.path.dirname(path)) and os.path.dirname(path) != '':
+                os.makedirs(os.path.dirname(path))
+            with open(path, 'w') as f:
+                json.dump(self.to_dict(), f)
