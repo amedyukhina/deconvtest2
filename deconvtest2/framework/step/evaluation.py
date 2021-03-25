624785a1b22876d239f344352ef36abdba1ef2a3
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-03-25 16:28:03 -0500

    added variables for number of inputs and outputs

--- deconvtest2/framework/step/evaluation.py
+++ deconvtest2/framework/step/evaluation.py
@@ -10,3 +10,5 @@ class Evaluation(Module):
         super(Evaluation, self).__init__(method=method,
                                          parameters=parameters,
                                          parent_name='deconvtest2.modules.evaluation')
+        self.n_inputs = 2
+        self.n_outputs = 0
