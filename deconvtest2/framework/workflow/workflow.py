ca878814d338b7bea7820022002b3ca97197ac24
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-03-25 21:53:02 -0500

    generate and save workflow graph

--- deconvtest2/framework/workflow/workflow.py
+++ deconvtest2/framework/workflow/workflow.py
@@ -3,10 +3,13 @@ import json
 import os
 import numpy as np
 from typing import Union
+import itertools
+import copy
 
 from .step import Step
 from ...core.utils.utils import list_modules
 from ...framework import step as workflow_steps
+from ...core.utils.conversion import keys_to_list
 
 
 def list_available_steps():
@@ -25,6 +28,7 @@ class Workflow:
         self.available_steps = list_available_steps()
         self.steps = []
         self.path = None
+        self.workflow = None
 
     def add_step(self, step: Step, input_step: Union[int, list] = None):
         if step.method is None:
@@ -82,3 +86,70 @@ class Workflow:
             s = Step(step['name'])
             s.from_dict(step)
             self.steps.append(s)
+
+    def get_workflow_graph(self, to_json=False):
+        blocks = []
+        for master_step in self.steps:
+            if master_step.name != 'Evaluation':
+                block = dict(name=rf'block{len(blocks):02d}')
+                block['items'] = []
+                for i in range(len(master_step.parameters)):
+                    item = dict(name=rf'item{i:02d}')
+                    item['steps'] = []
+                    step = dict(name=master_step.name, method=master_step.method)
+                    params = dict(master_step.parameters.iloc[i])
+                    for key in params.keys():
+                        try:
+                            step[key] = params[key].item()
+                        except AttributeError:
+                            step[key] = params[key]
+                    step['outputID'] = step.pop('ID')
+                    item['steps'].append(step)
+                    block['items'].append(item)
+                if len(master_step.parameters) == 0:
+                    item = dict(name=rf'item00')
+                    item['steps'] = []
+                    step = dict(name=master_step.name, method=master_step.method)
+                    step['outputID'] = ''
+                    item['steps'].append(step)
+                    block['items'].append(item)
+
+                if master_step.n_inputs > 0:
+                    lists = []
+                    for input_step in master_step.input_step:
+                        lists.append(blocks[input_step]['items'])
+                    if len(block['items']) > 0:
+                        lists.append(block['items'])
+
+                    nblock = dict(name=rf'updated_block{len(blocks):02d}')
+                    nblock['items'] = []
+                    for i, items in enumerate(itertools.product(*lists)):
+                        item = dict(name=rf'item{i:03d}')
+                        item['steps'] = []
+                        outputID = ''
+                        for iter_item in items:
+                            for st in iter_item['steps']:
+                                item['steps'].append(copy.deepcopy(st))
+                            outputID = outputID + iter_item['steps'][-1]['outputID'] + '_'
+                        nblock['items'].append(item)
+                        nblock['items'][i]['steps'][-1]['outputID'] = outputID.rstrip('_')
+                    blocks.append(nblock)
+                else:
+                    blocks.append(block)
+        self.workflow = blocks[-1]
+        if to_json:
+            return json.dumps(self.workflow, indent=4)
+        else:
+            return self.workflow
+
+    def save_workflow_graph(self, path):
+        if path is not None:
+            self.path = path
+
+        if self.path is None:
+            raise ValueError('Path must be provided!')
+        else:
+            workflow = self.get_workflow_graph()
+            with open(path, 'w') as f:
+                json.dump(workflow, f)
+
