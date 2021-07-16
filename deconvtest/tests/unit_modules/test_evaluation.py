81e3a9d1c5b682b0e307736e975a63d90274fadb
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-07-16 13:05:28 -0500

    test for NRMSE against skimage

--- deconvtest/tests/unit_modules/test_evaluation.py
+++ deconvtest/tests/unit_modules/test_evaluation.py
@@ -2,8 +2,9 @@ import unittest
 
 import numpy as np
 from ddt import ddt, data
+from skimage.metrics import normalized_root_mse
 
-from ...modules.evaluation.evaluation_modules import rmse
+from ...modules.evaluation.evaluation_modules import rmse, nrmse
 
 
 @ddt
@@ -12,12 +13,18 @@ class TestRMSE(unittest.TestCase):
     @data(
         (np.ones([10, 10, 10]), np.ones([10, 10, 10]), 0),
         (np.ones([10, 10, 10]), np.zeros([10, 10, 10]), 1),
+        (np.ones([10, 15, 10]), np.zeros([20, 10, 10]), 1),
 
     )
     def test_rmse(self, case):
         img1, img2, target_err = case
         err = rmse(img1, img2)
         self.assertEqual(err, target_err)
+
+    def test_against_skimage(self):
+        img1 = np.random.randint(0, 100, [100, 100])*1.
+        img2 = np.random.randint(0, 100, [100, 100])*1.
+        self.assertEqual(normalized_root_mse(img1, img2, normalization='min-max'), nrmse(img1, img2))
 
 
 if __name__ == '__main__':
