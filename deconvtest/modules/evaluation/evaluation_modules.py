2eb7d4b903dc99cbdf9332640e33efcde9d36119
Anna Medyukhina <anna.medyukhina@gmail.com>
2021-07-16 12:58:07 -0500

    added PSNR

--- deconvtest/modules/evaluation/evaluation_modules.py
+++ deconvtest/modules/evaluation/evaluation_modules.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.metrics import structural_similarity
+from skimage.metrics import peak_signal_noise_ratio
 
 from ...core.utils.conversion import unify_shape
 from ...core.utils.utils import check_type
@@ -67,3 +68,21 @@ def ssim(img1: np.ndarray, img2: np.ndarray) -> float:
     """
     return structural_similarity(img1*1., img2*1., full=False)
 
+
+def psnr(img1: np.ndarray, img2: np.ndarray) -> float:
+    """
+    Compute the peak signal to noise ratio (PSNR) for a test image
+
+    Parameters
+    ----------
+    img1 : ndarray
+        Ground truth image
+    img2 : numpy.ndarray
+        Test image
+
+    Returns
+    -------
+    float:
+        PSNR for the test image
+    """
+    return peak_signal_noise_ratio(img1*1., img2*1., data_range=np.max(img1)-np.min(img1))
