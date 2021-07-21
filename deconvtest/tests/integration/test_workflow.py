eac73dcc7c822a283532cc603bf326bcc3517cd0
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-07-21 15:16:27 -0500

    save output into a subfolder and remove after testing

--- deconvtest/tests/integration/test_workflow.py
+++ deconvtest/tests/integration/test_workflow.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 
 import numpy as np
@@ -11,19 +13,20 @@ from ...framework.workflow.workflow import Workflow
 class TestWorkflow(unittest.TestCase):
 
     def test_workflow(self):
-        w = Workflow(name='test workflow')
+        path = 'test_workflow'
+        w = Workflow(name='test workflow', output_path=os.path.join(path, 'data'))
 
         s = Step('GroundTruth', 'ellipsoid')
         path_gt = 'params_ellipsoid.csv'
         s.specify_parameters(size=[[10, 6, 6], 10], voxel_size=[[0.5, 0.2, 0.2]],
                              theta=[0, np.pi / 2], phi=[np.pi, np.pi * 4 / 3], mode='align', base_name='GT')
-        s.save_parameters(path_gt)
+        s.save_parameters(os.path.join(path, path_gt))
         w.add_step(s)
 
         s = Step('PSF', 'gaussian')
         path_psf = 'params_psf.csv'
         s.specify_parameters(sigma=[1, 2], aspect=[2, 4], mode='align')
-        s.save_parameters(path_psf)
+        s.save_parameters(os.path.join(path, path_psf))
         w.add_step(s)
 
         s = Step('Convolution', 'convolve')
@@ -33,21 +36,22 @@ class TestWorkflow(unittest.TestCase):
         s = Step('Transform', 'poisson_noise')
         s.specify_parameters(img='pipeline', snr=[2, 5], base_name='noise')
         path_noise = 'params_noise.csv'
-        s.save_parameters(path_noise)
+        s.save_parameters(os.path.join(path, path_noise))
         w.add_step(s)
 
         s = Step('Evaluation', 'rmse')
         s.specify_parameters(img1='pipeline', img2='pipeline')
         w.add_step(s, input_step=[0, 3])
 
-        path = 'workflow.json'
-        w.save(path)
+        wpath = 'workflow.json'
+        w.save(os.path.join(path, wpath))
         w2 = Workflow()
-        w2.load(path)
+        w2.load(os.path.join(path, wpath))
         path_graph = 'workflow_graph.json'
-        w2.save_workflow_graph(path_graph)
+        w2.save_workflow_graph(os.path.join(path, path_graph))
 
         w2.run()
+        shutil.rmtree(path)
 
 
 if __name__ == '__main__':
