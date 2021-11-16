b6d9a0c645655d51a9ebcc30bc2b2ca255d9783e
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-11-16 16:09:04 -0600

    module for organizing folders

---
+++ deconvtest/framework/module/organize.py
@@ -0,0 +1,19 @@
+from deconvtest.framework.module.align import Align
+
+
+class Organize(Align):
+    """
+    Transform module
+    """
+
+    def __init__(self, method: str = None,
+                 parameters: dict = None,
+                 parent_name: str = 'deconvtest.methods.organize'):
+        super(Organize, self).__init__(method=method,
+                                       parameters=parameters,
+                                       parent_name=parent_name)
+        self.n_inputs = 2
+        self.n_outputs = 2
+        self.type_input = ['image', 'image']
+        self.type_output = ['folder', 'folder']
+        self.align = True
