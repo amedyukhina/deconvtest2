b9651304e3011c24bc448946e69c3c09ba0aa6b1
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-11-17 14:26:14 -0600

    CARE datagen module

---
+++ deconvtest/methods/datagen/care_datagen.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+from typing import Union
+
+from csbdeep.data import RawData, create_patches
+
+
+def care_datagen(base_dir: str, n_patches_per_image: int, name_high: str = 'high', name_low: str = 'low',
+                 min_inputs: int = 0, axes: str = 'CZYX',
+                 patch_size: Union[int, list, tuple] = 10, verbose: bool = True):
+    while len(os.listdir(os.path.join(base_dir, name_high))) < min_inputs or \
+            len(os.listdir(os.path.join(base_dir, name_low))) < min_inputs:
+        sleep(1)
+
+    raw_data = RawData.from_folder(
+        basepath=base_dir,
+        source_dirs=[name_high],
+        target_dir=name_low,
+        axes=axes,
+    )
+
+    patches = create_patches(raw_data, n_patches_per_image=n_patches_per_image,
+                             patch_size=patch_size, verbose=verbose)
+    return patches
