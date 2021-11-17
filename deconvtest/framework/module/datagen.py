cfef86aa5e649cf45f889f5b56eebe2faa8633d1
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-11-17 14:25:56 -0600

    add id = True

--- deconvtest/framework/module/datagen.py
+++ deconvtest/framework/module/datagen.py
@@ -9,11 +9,10 @@ class DataGen(Transform):
     def __init__(self, method: str = None, parameters: dict = None,
                  parent_name: str = 'deconvtest.methods.datagen'):
         super(DataGen, self).__init__(method=method,
-                                        parameters=parameters,
-                                        parent_name=parent_name)
+                                      parameters=parameters,
+                                      parent_name=parent_name)
         self.n_inputs = 1
         self.n_outputs = 1
         self.type_input = 'folder'
         self.type_output = 'data'
         self.wait_complete = True
-        self.add_id = False
