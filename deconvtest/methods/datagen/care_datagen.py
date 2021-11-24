0e684b6bb062d805cb591adfc77c1738e08f7f92
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-11-24 15:19:59 -0600

    write data inside the datagen function

--- deconvtest/methods/datagen/care_datagen.py
+++ deconvtest/methods/datagen/care_datagen.py
@@ -6,7 +6,7 @@ from csbdeep.data import RawData, create_patches
 
 
 def care_datagen(base_dir: str, n_patches_per_image: int, name_high: str = 'high', name_low: str = 'low',
-                 min_inputs: int = 0, axes: str = 'CZYX',
+                 min_inputs: int = 0, axes: str = 'CZYX', fn_output: str = 'test',
                  patch_size: Union[int, list, tuple] = 10, verbose: bool = True):
     while len(os.listdir(os.path.join(base_dir, name_high))) < min_inputs or \
             len(os.listdir(os.path.join(base_dir, name_low))) < min_inputs:
@@ -19,6 +19,6 @@ def care_datagen(base_dir: str, n_patches_per_image: int, name_high: str = 'high
         axes=axes,
     )
 
-    patches = create_patches(raw_data, n_patches_per_image=n_patches_per_image,
-                             patch_size=patch_size, verbose=verbose)
-    return patches
+    create_patches(raw_data, n_patches_per_image=n_patches_per_image,
+                   patch_size=patch_size, verbose=verbose, save_file=fn_output)
+
