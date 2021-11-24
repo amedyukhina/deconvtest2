1af252968e4cd93e12a790295a2f62307e61c1df
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-11-24 15:20:18 -0600

    rename output dir

--- deconvtest/methods/organize/care_prep.py
+++ deconvtest/methods/organize/care_prep.py
@@ -7,9 +7,9 @@ from ...core.utils.conversion import unify_shape
 
 def care_prep(img_high: str, img_low: str,
               name_high: str = 'high', name_low: str = 'low',
-              output_dir: str = 'test', img_name: str = 'test.tif'):
+              fn_output: str = 'test', img_name: str = 'test.tif'):
     img_high, img_low = unify_shape(img_high, img_low)
-    os.makedirs(os.path.join(output_dir, name_low), exist_ok=True)
-    os.makedirs(os.path.join(output_dir, name_high), exist_ok=True)
-    io.imsave(os.path.join(output_dir, name_high, img_name), img_high)
-    io.imsave(os.path.join(output_dir, name_low, img_name), img_low)
+    os.makedirs(os.path.join(fn_output, name_low), exist_ok=True)
+    os.makedirs(os.path.join(fn_output, name_high), exist_ok=True)
+    io.imsave(os.path.join(fn_output, name_high, img_name), img_high)
+    io.imsave(os.path.join(fn_output, name_low, img_name), img_low)
