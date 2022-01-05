bb07af140b10e127be6f31d7fc3198407f9971b2
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2022-01-05 15:25:28 -0600

    unit tests for ssim and psnr

--- deconvtest/tests/unit_methods/test_evaluation.py
+++ deconvtest/tests/unit_methods/test_evaluation.py
@@ -2,10 +2,12 @@ import unittest
 
 import numpy as np
 from ddt import ddt, data
-from skimage.metrics import normalized_root_mse
+from skimage.metrics import normalized_root_mse, structural_similarity, peak_signal_noise_ratio
 
 from ...methods.evaluation.nrmse import nrmse
 from ...methods.evaluation.rmse import rmse
+from ...methods.evaluation.psnr import psnr
+from ...methods.evaluation.ssim import ssim
 
 
 @ddt
@@ -21,11 +23,15 @@ class TestRMSE(unittest.TestCase):
         img1, img2, target_err = case
         err = rmse(img1, img2)
         self.assertEqual(err, target_err)
+        self.assertAlmostEqual(ssim(img1, img2), 1 - target_err, 2)
 
     def test_against_skimage(self):
         img1 = np.random.randint(0, 100, [100, 100]) * 1.
         img2 = np.random.randint(0, 100, [100, 100]) * 1.
         self.assertEqual(normalized_root_mse(img1, img2, normalization='min-max'), nrmse(img1, img2))
+        self.assertEqual(structural_similarity(img1, img2, full=False), ssim(img1, img2))
+        self.assertEqual(peak_signal_noise_ratio(img1, img2, data_range=np.max(img1) - np.min(img2)),
+                         psnr(img1, img2))
 
 
 if __name__ == '__main__':
