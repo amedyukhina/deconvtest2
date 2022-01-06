cc4c5bbe7343ad854bda0d6151be0e5a28447128
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2022-01-06 13:54:32 -0600

    constants for file paths

--- deconvtest/core/utils/constants.py
+++ deconvtest/core/utils/constants.py
@@ -5,3 +5,8 @@ EXTENSIONS = dict(image='.tif',
                   file='',
                   data='.npz',
                   model='')
+DEFAULT_WORKFLOW_NAME = 'New Workflow'
+DATA_FOLDER_NAME = 'data'
+PARAMETER_FOLDER_NAME = 'parameters'
+WORKFLOW_FN = 'workflow.json'
+WORKFLOW_GRAPH_FN = 'workflow_graph.json'
